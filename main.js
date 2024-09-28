ebabff5fb57745a57c1caa5f60a95ef5b9e5f3e7
doyoung1002 <leedoyoung.oc@gmail.com>
2024-09-28 19:49:23 +0900

    select, localstorage, 유효성 검사, input 구현

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const resultElement = document.querySelector('#result');
 const formContainer = document.querySelector('#form');
 const productList = document.querySelector('#product-list')
 const productSelect = document.querySelector('#product-select');
-
+const productTotal = document.querySelector('#product-total');
+const productButton = document.querySelector('#product-btn');
 
 function Product(name, price) {
   this.name = name;
@@ -18,24 +19,56 @@ const products = [
   new Product('중뱃살', 1000),
 ]
 
-// products.length 만큼 반복문 돌려서 상품 list 출력
-products.forEach(product => {
-  const option = document.createElement('option');
-  option.textContent = `${product.name} - ${product.price}`;
-  option.value = `${product.name} - ${product.price}`
-  productList.appendChild(option);
+const addPayment = function () {
+  window.open('payment.html', '_blank', 'left=100, top=100, width=500, height=300');
+}
+
+const totalPriceData = function () {
+  localStorage.setItem('totalPrice', totalPrice)
+}
+
+let totalPrice = 0;
+
+const productAllList = function () {
+  products.forEach(product => {
+    const option = document.createElement('option');
+    option.textContent = `${product.name} - ${product.price}`;
+    option.value = JSON.stringify(product);
+    productList.appendChild(option);
+  })
+}
+
+const updateProduct = function () {
+  productList.addEventListener('change', function () {
+    const selectList = Array.from(productList.selectedOptions).map(option => JSON.parse(option.value));
+    productSelect.innerHTML = '';
+    totalPrice = 0;
+
+    selectList.forEach(product => {
+      productSelect.classList.remove('hidden');
+      const buyList = document.createElement('li');
+      buyList.textContent = `${product.name} - ${product.price}`;
+      productSelect.appendChild(buyList);
+      totalPrice += product.price;
+    })
+
+    productTotal.textContent = `총액 ${totalPrice}원`;
+  });
+}
+
+productButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (totalPrice === 0) {
+    alert('구매할 상품을 선택하세요');
+  } else {
+    totalPriceData();
+    addPayment();
+  }
 })
 
-productList.addEventListener('change', function () {
-  const selectedOptions = productList.selectedOptions;
+const init = function () {
+  productAllList();
+  updateProduct();
+}
 
-  productSelect.classList.remove('hidden');
-  const buyList = document.createElement('li');
-  buyList.textContent = productList.value;
-  productSelect.appendChild(buyList);
-})
-
-// const productText = document.createElement('li');
-// productText.setAttribute('class', 'select-result');
-// productText.appendChild(document.createTextNode(productList.options[productList.selectedIndex].text));
-// productSelect.appendChild(productText);
+init()
